d4acd86a1969bcfe3c4e85e5749c874301f7bc4c
nyomathys <nyo.mathys@student.ehb.be>
2024-12-10 14:11:35 +0100

    Header & Footer made for all current pages

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,11 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 //Settings page
 
 const Settings = () => {
   return (
     <div>
+      <Header />
       <h1>Settings</h1>
+      <Footer />
     </div>
   );
 };
-
-export default Settings;
--- src/pages/Social.js
+++ src/pages/Social.js
@@ -1,11 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 //Social feed page
 
 const Social = () => {
   return (
     <div>
+      <Header />
       <h1>Social</h1>
+      <Footer />
     </div>
   );
 };
 
-export default Social;
