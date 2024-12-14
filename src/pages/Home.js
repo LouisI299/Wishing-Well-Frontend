d8eecb206ee197e7fbac6cb2e66634b5caa54714
LouisI299 <louisingelbrecht@gmail.com>
2024-12-14 13:36:44 +0100

    Cards for goals, styled home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,24 @@
 //Imports
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Alert } from "react-bootstrap";
+import {
+  Alert,
+  Card,
+  CardHeader,
+  CardBody,
+  CardImg,
+  ProgressBar,
+} from "react-bootstrap";
 import { fetchData } from "../utils/api";
 import { useAuth } from "../contexts/AuthProvider";
+import {
+  StyledCard,
+  HomeContainer,
+  Chevron,
+  GoalProgress,
+} from "../styles/HomeStyles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarAlt, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   //State
@@ -13,6 +28,14 @@ const Home = () => {
   const { token } = useAuth();
   const location = useLocation();
   const successMessage = location.state?.successMessage;
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Months are zero-based
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
 
   //Get goals from api.js
   useEffect(() => {
@@ -30,22 +53,54 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h1>Home</h1>
+    <HomeContainer>
       {successMessage && <Alert variant="success">{successMessage}</Alert>}
       {/* Display each goal */}
-      <ul>
-        {goals.map((goal) => (
-          <li key={goal.id}>
-            <Link to={`/goal-summary/${goal.id}`}>
-              {goal.name}: {goal.status}. Target amount: {goal.target_amount},
-              Current amount: {goal.current_amount}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+      {goals.map((goal) => {
+        const progress = (goal.current_amount / goal.target_amount) * 100;
+        return (
+          <Link to={`/goal-summary/${goal.id}`} key={goal.id}>
+            <StyledCard>
+              <CardImg
+                className="card-img"
+                src="https://via.placeholder.com/150"
+                alt="Placeholder"
+              />
+              <CardHeader>
+                <p className="title">{goal.name}</p>
+                <p>
+                  <FontAwesomeIcon icon={faCalendarAlt} />{" "}
+                  {formatDate(goal.end_date)}
+                </p>
+              </CardHeader>
+
+              <CardBody>
+                <div id="targetDiv">
+                  <p>Target:</p>
+                </div>
+                <div id="progressTargetDiv">
+                  <GoalProgress>
+                    <ProgressBar
+                      now={progress}
+                      label={`${progress.toFixed(2)}%`}
+                    />
+
+                    <Chevron progress={progress}>
+                      <FontAwesomeIcon
+                        icon={faChevronUp}
+                        className="chevron-icon"
+                      />
+                      {goal.current_amount}€
+                    </Chevron>
+                  </GoalProgress>
+                  <p>{goal.target_amount}€</p>
+                </div>
+              </CardBody>
+            </StyledCard>
+          </Link>
+        );
+      })}
+    </HomeContainer>
   );
 };
-
 export default Home;
