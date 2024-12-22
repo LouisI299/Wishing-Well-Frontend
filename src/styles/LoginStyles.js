75dae2e588034ebe8d02d986913cf361895cee73
edsondes <edson.de.smet@student.ehb.be>
2024-12-22 19:02:39 +0100

    Refactor login and register styles for improved responsiveness and layout

--- src/styles/LoginStyles.js
+++ src/styles/LoginStyles.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
-import { Container, Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Form } from "react-bootstrap";
+import styled from "styled-components";
 
 export const LogoContainer = styled(Container)`
   max-width: 100%;
@@ -9,6 +9,23 @@ export const LogoContainer = styled(Container)`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+ img {
+    width: 100px;
+  }
+
+  h1 {
+    font-size: 2em;
+  }
+
+   @media (min-width: 1024px) {
+    img {
+      width: 150px;
+      margin: auto;
+    }
+    h1 {
+      font-size: 2.5em;
+    }
+  }
 `;
 
 export const StyledFormGroup = styled(Form.Group)`
@@ -17,6 +34,11 @@ export const StyledFormGroup = styled(Form.Group)`
   height: 3em;
   margin: 1em 0 1em 0;
   position: relative;
+
+   @media (max-width: 768px) {
+    height: 2.5em;
+    margin: 0.5em 0;
+  }
 `;
 
 export const InputIcon = styled(FontAwesomeIcon)`
@@ -32,12 +54,19 @@ export const InputGroup = styled.div`
   align-items: center;
   position: relative;
   width: 100%;
+
+   @media (min-width: 1024px) {
+    width: 50%;
+    margin-bottom: 1em;
+  }
 `;
 
 export const StyledInput = styled(Form.Control)`
   padding-left: 35px;
   height: 3em;
   border-radius: 1em;
+
+  
 `;
 
 export const ButtonContainer = styled.div`
@@ -52,8 +81,15 @@ export const ButtonContainer = styled.div`
     margin: 1em 0 1.5em 0;
     font-size: 1.3em;
     border-radius: 2em;
+    text-align: center;
   }
-
+   @media (min-width: 768px) {
+      width: 40%;
+      font-size: 1em;
+      margin: auto;
+      
+    }
+  }
   a {
     text-decoration: none;
     color: ${({ theme }) => theme.text};
--- src/styles/RegisterStyles.js
+++ src/styles/RegisterStyles.js
@@ -1,12 +1,17 @@
-import styled from "styled-components";
-import { Container, Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container, Form } from "react-bootstrap";
+import styled from "styled-components";
 
 export const StyledIcon = styled(FontAwesomeIcon)`
   color: ${({ theme }) => theme.text};
 
   font-size: 2em;
   padding: 0.5em;
+
+  @media (min-width: 1024px) {
+    font-size: 1.5em;
+  }
+   
 `;
 
 export const IconContainer = styled.div`
@@ -20,17 +25,21 @@ export const IconContainer = styled.div`
   border-width: 2px;
   display: flex;
   justify-content: center;
+
 `;
 
 export const RegisterContainer = styled(Container)`
-  padding: 0;
   width: 100%;
 `;
 
 export const StyledInput = styled(Form.Control)`
   height: 3em;
   margin: 0.5em 0 0.5em 0;
-  border-radius: 1em;
+  border-radius: 1em;  
+  @media (min-width: 1024px) {
+  width: 50%;
+  margin: 1rem auto 1rem auto;
+}
 `;
 
 export const ButtonContainer = styled.div`
@@ -45,6 +54,12 @@ export const ButtonContainer = styled.div`
     margin: 1em 0 1.5em 0;
     font-size: 1.3em;
     border-radius: 2em;
+
+     @media (min-width: 1024px) {
+      width: 30%;
+      font-size: 1.5em;
+      margin: 1.5em 0;
+    }
   }
 
   a {
